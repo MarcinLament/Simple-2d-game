2b4a37ff64cc95df5cbf70692801108c9491992e
Marcin work <lammar1987@gmail.com>
2012-04-12 14:12:42 +0100

    deleted some files

--- Cocos2dxSimpleGame/android/obj/convert-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# AUTO-GENERATED FILE, DO NOT EDIT!
-if [ -f $1.org ]; then
-  sed -e 's!^W:/Tools/cygwin/lib!/usr/lib!ig;s! W:/Tools/cygwin/lib! /usr/lib!ig;s!^W:/Tools/cygwin/bin!/usr/bin!ig;s! W:/Tools/cygwin/bin! /usr/bin!ig;s!^W:/Tools/cygwin/!/!ig;s! W:/Tools/cygwin/! /!ig;s!^Y:!/cygdrive/y!ig;s! Y:! /cygdrive/y!ig;s!^W:!/cygdrive/w!ig;s! W:! /cygdrive/w!ig;s!^C:!/cygdrive/c!ig;s! C:! /cygdrive/c!ig;' $1.org > $1 && rm -f $1.org
-fi
--- Cocos2dxSimpleGame/android/obj/local/armeabi/objs-debug/cocos2d/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-/cygdrive/w/GameSamples/JACO_Cocos2dX/Simple-2d-game/Cocos2dxSimpleGame/android/obj/local/armeabi/objs-debug/cocos2d/support/ccUtils.o: \
- /cygdrive/w/GameSamples/JACO_Cocos2dX/Simple-2d-game/Cocos2dxSimpleGame/android/jni/../../../cocos2dx/support/ccUtils.cpp \
- /cygdrive/w/GameSamples/JACO_Cocos2dX/Simple-2d-game/Cocos2dxSimpleGame/android/jni/../../../cocos2dx/support/ccUtils.h
-
-/cygdrive/w/GameSamples/JACO_Cocos2dX/Simple-2d-game/Cocos2dxSimpleGame/android/jni/../../../cocos2dx/support/ccUtils.h:
